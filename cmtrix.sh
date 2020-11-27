5b88b75ad03e0fd01292f6e7f9bf8e5cf89e6300
roy-code <71874823+roy-code@users.noreply.github.com>
2020-11-27 08:20:27 +0800

    Add files via upload

---
+++ cmtrix.sh
@@ -0,0 +1,42 @@
+Skip to content
+Rayduan Skript / Attack
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+echo " "
+echo " "
+echo '
+                  _  _  ___  ___   __  ____  ___ 
+                 ( )( )(  ,\(   \ (  )(_  _)(  _)
+                  )()(  ) _/ ) ) )/__\  )(   ) _)
+                   v 1.0
+'|lolcat
+echo " "
+echo " "
+sleep 6.0
+clear
+echo -e "$red                         ChEcKiNg..>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg...>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg....>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg.....>$rset"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo -e "$grn                updating the cmatrix  please wait$rset"
+sleep 2.0
+cd $HOME
+pkg install cmatrix
+cmatrix
+
+
